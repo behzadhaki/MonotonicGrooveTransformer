dc473e9c8caf5185eb03005cc70f8b662346b036
behzadhaki <35939495+behzadhaki@users.noreply.github.com>
2022-02-26 16:21:44 +0100

    first working version

--- Lab - Monotonic Groove Transformer/main.py
+++ Lab - Monotonic Groove Transformer/main.py
@@ -1,5 +1,5 @@
 # Import model
-from utils import load_model
+from utils import load_model, get_new_drum_osc_msgs
 import torch
 
 
@@ -9,25 +9,91 @@ from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
 
 if __name__ == '__main__':
+    # ------------------ Load Trained Model  ------------------ #
     model_name = "groove_transformer_trained"
     model_path = f"trained_torch_models/{model_name}.model"
 
     groove_transformer = load_model(model_name, model_path)
 
-    # Create an empty an empty torch tensor
+    # ------  Create an empty an empty torch tensor
     input_tensor = torch.zeros((1, 32, 27))
+
+    # ------  Create an empty h, v, o tuple for previously generated events to avoid duplicate messages
+    (h_old, v_old, o_old) = (torch.zeros((1, 32, 9)), torch.zeros((1, 32, 9)), torch.zeros((1, 32, 9)))
 
     # get velocity and timing
     groove_velocities = torch.rand((32))
     groove_timings = -0.5 + torch.rand((32))
 
-    input_tensor[0, :, 2] = torch.where(groove_velocities > 0, 1, 0)    # set hits
-    input_tensor[0, :, 11] = groove_velocities                          # set velocities
-    input_tensor[0, :, 20] = groove_timings                             # set microtimings
+    """
+    input_tensor[0, :, 2] = torch.where(groove_velocities > 0, 1, 0)  # set hits
+    input_tensor[0, :, 11] = groove_velocities  # set velocities
+    input_tensor[0, :, 20] = groove_timings  # set microtimings
 
-    h, v, o = groove_transformer.predict(input_tensor)
+    h, v, o = groove_transformer.predict(input_tensor)"""
 
-    print(h)
+
+
+    # ----------------------------------------------------------
+
+    # ------------------ OSC ips / ports ------------------ #
+    # connection parameters
+    ip = "127.0.0.1"
+    receiving_from_pd_port = 1415
+    sending_to_pd_port = 1123
+
+    # ----------------------------------------------------------
+
+    # ------------------ OSC Receiver from Pd ------------------ #
+    # create an instance of the osc_sender class above
+    py_to_pd_OscSender = SimpleUDPClient(ip, sending_to_pd_port)
+    # ---------------------------------------------------------- #
+
+    # ------------------ OSC Receiver from Pd ------------------ #
+    # dispatcher is used to assign a callback to a received osc message
+    # in other words the dispatcher routes the osc message to the right action using the address provided
+    dispatcher = Dispatcher()
+
+
+    # define the handler for messages starting with /velocity]
+    def groove_event_handler(address, *args):
+        input_tensor[:, int(args[2]), 2] = 1 if args[0] > 0 else 0               # set hit
+        input_tensor[:, int(args[2]), 11] = args[0]  / 127      # set velocity
+        input_tensor[:, int(args[2]), 20] = args[1]          # set utiming
+        print(input_tensor[:, :, 9:18])
+
+    # pass the handlers to the dispatcher
+    dispatcher.map("/VelutimeIndex*", groove_event_handler)
+
+
+    # you can have a default_handler for messages that don't have dedicated handlers
+    def default_handler(address, *args):
+        print(f"No action taken for message {address}: {args}")
+
+    dispatcher.set_default_handler(default_handler)
+
+    # python-osc method for establishing the UDP communication with pd
+    server = BlockingOSCUDPServer((ip, receiving_from_pd_port), dispatcher)
+    # ---------------------------------------------------------- #
+
+    # ------------------ NOTE GENERATION  ------------------ #
+    drum_voice_pitch_map = {"kick": 36, 'snare': 38, 'tom-1': 47, 'tom-2': 42, 'chat': 64, 'ohat': 63}
+    drum_voices = list(drum_voice_pitch_map.keys())
+
+    while (1):
+        server.handle_request()
+        # get new generated pattern
+        h_new, v_new, o_new = groove_transformer.predict(input_tensor, thres=0.1)
+
+        # send to pd
+        osc_messages_to_send = get_new_drum_osc_msgs((h_new, v_new, o_new))
+        print("RESET TABLE!")
+        py_to_pd_OscSender.send_message("/reset_table", 1)
+        for (address, h_v_ix_tuple) in osc_messages_to_send:
+            py_to_pd_OscSender.send_message(address, h_v_ix_tuple)
+
+        # update old values with the sent ones
+        # (h_old, v_old, o_old) = (h_new, v_new, o_new)
 
 """
 if __name__ == '__main__':
--- Lab - Monotonic Groove Transformer/utils.py
+++ Lab - Monotonic Groove Transformer/utils.py
@@ -3,6 +3,19 @@ import torch
 from trained_torch_models.params import model_params
 from BaseGrooveTransformers.models.transformer import GrooveTransformerEncoder
 
+# MAGENTA MAPPING
+ROLAND_REDUCED_MAPPING = {
+    "KICK": [36],
+    "SNARE": [38, 37, 40],
+    "HH_CLOSED": [42, 22, 44],
+    "HH_OPEN": [46, 26],
+    "TOM_3_LO": [43, 58],
+    "TOM_2_MID": [47, 45],
+    "TOM_1_HI": [50, 48],
+    "CRASH": [49, 52, 55, 57],
+    "RIDE":  [51, 53, 59]
+}
+ROLAND_REDUCED_MAPPING_VOICES = ['/KICK', '/SNARE', '/HH_CLOSED', '/HH_OPEN', '/TOM_3_LO', '/TOM_2_MID', '/TOM_1_HI', '/CRASH', '/RIDE']
 
 def load_model(model_name, model_path):
 
@@ -29,5 +42,35 @@ def load_model(model_name, model_path):
 
     return groove_transformer
 
-def place_note_in_tensor():
-    pass
+def get_new_drum_osc_msgs(hvo_tuple_new, hvo_tuple_prev=None):
+    message_list = list()
+    (h_new, v_new, o_new) = hvo_tuple_new
+    hit_locations_new = torch.nonzero(h_new)
+
+    for batch_ix, timestep, voiceIx  in hit_locations_new:
+        # timestep = int(timestep)
+        # voiceIx = int(voiceIx)
+        if hvo_tuple_prev is not None:
+            if (hvo_tuple_prev[0][batch_ix, timestep, voiceIx] != h_new[batch_ix, timestep, voiceIx] or
+                    hvo_tuple_prev[1][batch_ix, timestep, voiceIx] != v_new[batch_ix, timestep, voiceIx] or
+                    hvo_tuple_prev[2][batch_ix, timestep, voiceIx] != o_new[batch_ix, timestep, voiceIx]):
+                address = ROLAND_REDUCED_MAPPING_VOICES[voiceIx]
+                osc_msg = (
+                        "/drum_generated".join(address),
+                        (v_new[batch_ix, timestep, voiceIx].item(), o_new[batch_ix, timestep, voiceIx].item(),
+                        timestep.item())
+                    )
+                message_list.append(osc_msg)
+        else:
+            address = ROLAND_REDUCED_MAPPING_VOICES[voiceIx]
+            osc_msg = (
+                "/drum_generated"+address,
+                (v_new[batch_ix, timestep, voiceIx].item(), o_new[batch_ix, timestep, voiceIx].item(),
+                 timestep.item())
+            )
+            message_list.append(osc_msg)
+
+            print("OSC msg to send: ", osc_msg)
+
+    return message_list
+
