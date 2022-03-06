b7741da852fb75485e88a3aff9b1023465b1b049
behzadhaki <35939495+behzadhaki@users.noreply.github.com>
2022-03-06 15:42:27 +0100

    python script updated. osc received on background

--- Lab - Monotonic Groove Transformer/main.py
+++ Lab - Monotonic Groove Transformer/main.py
@@ -1,5 +1,5 @@
 # Import model
-from utils import load_model, get_new_drum_osc_msgs, get_prediction
+from utils import load_model, get_new_drum_osc_msgs, get_prediction, OscMessageReceiver
 import torch
 
 
@@ -7,6 +7,7 @@ import torch
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
+import queue
 
 # Parser for terminal commands
 import argparse
@@ -48,6 +49,7 @@ if __name__ == '__main__':
     ip = "127.0.0.1"
     receiving_from_pd_port = args.pd2py_port
     sending_to_pd_port = args.py2pd_port
+    message_queue = queue.Queue()
     # ----------------------------------------------------------
 
     # ------------------ OSC Receiver from Pd ------------------ #
@@ -55,38 +57,20 @@ if __name__ == '__main__':
     py_to_pd_OscSender = SimpleUDPClient(ip, sending_to_pd_port)
     # ---------------------------------------------------------- #
 
-    # ------------------ OSC Receiver from Pd ------------------ #
-    # dispatcher is used to assign a callback to a received osc message
-    # in other words the dispatcher routes the osc message to the right action using the address provided
-    dispatcher = Dispatcher()
-
-    # define the handler for messages starting with /velocity]
-    def groove_event_handler(address, *args):
-        input_tensor[:, int(args[2]), 2] = 1 if args[0] > 0 else 0               # set hit
-        input_tensor[:, int(args[2]), 11] = args[0]  / 127      # set velocity
-        input_tensor[:, int(args[2]), 20] = args[1]          # set utiming
-
-    # define the handler for sampling thresholds
-    def sampling_threshold_handler(address, *args):
-        voice_thresholds[int(address.split("/")[-1])] = 1-args[0]
-
-    # define the handler for sampling thresholds
-    def max_count_handler(address, *args):
-        voice_max_count_allowed[int(address.split("/")[-1])] = int(args[0])
-
-    # pass the handlers to the dispatcher
-    dispatcher.map("/VelutimeIndex*", groove_event_handler)
-    dispatcher.map("/threshold*", sampling_threshold_handler)
-    dispatcher.map("/max_count*", max_count_handler)
-
-    # you can have a default_handler for messages that don't have dedicated handlers
-    def default_handler(address, *args):
-        print(f"No action taken for message {address}: {args}")
-
-    dispatcher.set_default_handler(default_handler)
+    def process_message_from_queue(address, args):
+        if "VelutimeIndex" in address:
+            input_tensor[:, int(args[2]), 2] = 1 if args[0] > 0 else 0  # set hit
+            input_tensor[:, int(args[2]), 11] = args[0] / 127  # set velocity
+            input_tensor[:, int(args[2]), 20] = args[1]  # set utiming
+        if "threshold" in address:
+            voice_thresholds[int(address.split("/")[-1])] = 1-args[0]
+        if "max_count" in address:
+            voice_max_count_allowed[int(address.split("/")[-1])] = int(args[0])
 
     # python-osc method for establishing the UDP communication with pd
-    server = BlockingOSCUDPServer((ip, receiving_from_pd_port), dispatcher)
+    server = OscMessageReceiver(ip, receiving_from_pd_port, message_queue=message_queue)
+    server.start()
+
     # ---------------------------------------------------------- #
 
     # ------------------ NOTE GENERATION  ------------------ #
@@ -94,22 +78,27 @@ if __name__ == '__main__':
     drum_voices = list(drum_voice_pitch_map.keys())
     
     number_of_generations = 0
+    count = 0
     while (1):
-        server.handle_request()
-        # get new generated pattern
-        # h_new, v_new, o_new = groove_transformer.predict(input_tensor, thres=0.5)
-        h_new, v_new, o_new = get_prediction(groove_transformer, input_tensor, voice_thresholds, voice_max_count_allowed)
-        _h, v, o = groove_transformer.forward(input_tensor)
+        address, args = message_queue.get()
+        process_message_from_queue(address, args)
 
-        # send to pd
-        osc_messages_to_send = get_new_drum_osc_msgs((h_new, v_new, o_new))
-        number_of_generations += 1
-        
-        # First clear generations on pd by sending a message
-        py_to_pd_OscSender.send_message("/reset_table", 1)
-        
-        # Then send over generated notes one at a time
-        for (address, h_v_ix_tuple) in osc_messages_to_send:
-            py_to_pd_OscSender.send_message(address, h_v_ix_tuple)
+        # only generate new pattern when there isnt any other osc messages backed up for processing in the message_queue
+        if (message_queue.qsize() == 0):
+            # h_new, v_new, o_new = groove_transformer.predict(input_tensor, thres=0.5)
+            h_new, v_new, o_new = get_prediction(groove_transformer, input_tensor, voice_thresholds,
+                                                 voice_max_count_allowed)
+            _h, v, o = groove_transformer.forward(input_tensor)
+
+            # send to pd
+            osc_messages_to_send = get_new_drum_osc_msgs((h_new, v_new, o_new))
+            number_of_generations += 1
+
+            # First clear generations on pd by sending a message
+            py_to_pd_OscSender.send_message("/reset_table", 1)
+
+            # Then send over generated notes one at a time
+            for (address, h_v_ix_tuple) in osc_messages_to_send:
+                py_to_pd_OscSender.send_message(address, h_v_ix_tuple)
             
-        
+            count += 1
